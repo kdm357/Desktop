c98b3892f2bb8ea9fe56782430d8b29190d8cdf4
kdm357 <keith_moore7@msn.com>
2019-08-05 10:44:42 -0600

    test changes

---
+++ numbers.py
@@ -0,0 +1,33 @@
+# print("Hello Python")
+
+# Scalar data types: Integer, Floating point
+# None, and Boolean
+
+print("Binary notation", 0b0100)
+print("Hex notation", 0b0100)
+#  Use the int() constructor
+print("477")
+# Option for Base
+print (int("1001", 2))
+
+# Floating point
+#Scientific Notation
+print("Scientific Notation", 1.6e-35)
+print("Float construction float()", float("1.99"))
+
+# Boolean
+print("Boolean const. bool()", bool(0))
+print("Boolean const. bool()", bool(42))
+
+# TEst Boolean on collection
+print("Boolean const. bool([])", bool([]))
+print("Boolean const. bool([])", bool([1,2,3,4]))
+
+# Relational operator:  ==, !=, <, >,
+a=12
+print("a == 12", a ==12)
+
+
+
+
+
