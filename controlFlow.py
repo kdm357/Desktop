7013a6be288ec6d4d7dfdb940a585ee340fcfa0f
kdm357 <keith_moore7@msn.com>
2019-08-05 11:06:14 -0600

    test changes

--- controlFlow.py
+++ controlFlow.py
@@ -6,4 +6,22 @@ Learn about control flow in Python
 if True:
     print("It is True")
 
+if bool("eggs"):
+    print("Yes Please")
+
+if "eggs":
+    print("Yes, Why not")
+
+
+# Flat is better than nested
+h=42
+if h>50:
+    print("Greater than 50")
+    if h>100:
+        print("Greater than 100")
+elif h<50:
+    print("Less than 50")
+else:
+    print("equals 50")
+
 print("Done")
