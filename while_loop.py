7fb59941966da2642ead9dab60ae8c593596062b
kdm357 <keith_moore7@msn.com>
2019-08-05 11:13:14 -0600

    test changes

---
+++ while_loop.py
@@ -0,0 +1,18 @@
+"""
+Learn condittional repition
+Two loops: For loopa and while loops
+"""
+
+counter = 5
+while counter != 0:
+    print(counter)
+    # Augmented Operator
+    counter -= 1
+
+counter = 5
+while counter:
+    print(counter)
+    # Augmented Operator
+    counter -= 1
+
+print("outside while loop")
