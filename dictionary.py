6f58773c9ffb65cabbc305c6519964fcc9eeded8
kdm357 <keith_moore7@msn.com>
2019-08-05 13:44:29 -0600

    more changes

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+"""
+learn dictionaries
+dictionaries maps keys to values.
+
+in some languages are known as asscoiative arrays, or hashs, maps
+
+create then using {} containing a key-value pair
+retrieve them by [key values]
+"""
+
+d = {'alice':'801-555-4452154',
+     'pedro':'985-658-55874',
+     'john':'9985-5587-885478'}
+print(d, type(d))
+
+#access an element
+print(d['pedro'])
