022c737ff6573758f67a0b2b8434ebe8f6251636
kdm357 <keith_moore7@msn.com>
2019-08-05 11:31:52 -0600

    test changes

---
+++ strings.py
@@ -0,0 +1,26 @@
+"""
+Strings and Collections
+
+A string is an immutable sequence of Unicode code-poinnts
+"""
+
+print ('This is a string')
+print ("This is a string too")
+
+#concatenation and adjacent strings
+s1 = "First"
+s2 = "Second"
+print(s1 + s2)
+
+# multiline strings
+s3 = """This is
+a multiline
+string"""
+print(s3)
+
+s4 = "This is\na multiple-line\nString"
+print(s4)
+
+# support fo rbackslash
+s5 = "A\\in a string"
+print(s5)
