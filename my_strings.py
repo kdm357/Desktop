4bb896f021c3e00d0f2cc7949473e3dab4085bc6
keithmoore1 <keith_moore7@msn.com>
2019-08-06 15:58:33 -0600

    Day 2

--- my_strings.py
+++ my_strings.py
@@ -10,6 +10,26 @@ def main():
     :return: nohing
     """
     s1 = "This is super cool"
+    print("Size of S1: ", len(s1))
+    #concatenation
+    s2 = "Weber" + "State" + "University"
+    print(s2)
+    # to join large strings use join
+    teams  = ["Real Madrid","Barcelona","Manchester United"]
+    record = ":".join(teams)
+    # print (record)
+    # Split record
+    print(record.split(":"))
+    # partitioning strings
+    # you can use the dummy object ("_")
+    departure, _, arrival = "London:Edinburg".partition(":")
+    print(departure, arrival)
+    # String formatting using format method
+    print("The age of {0} is {1}".format("Mario", 34))
+    #ommiting the index
+    print("The best numbers are {} and {}".format(4,22))
+    # or by fieldname
+    print("Current position {latitude} {longitude}".format(latitude="60 N", longitude="5 E"))
 
 
 if __name__ == '__main__':
