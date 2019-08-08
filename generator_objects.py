bf7e5c0baa137cfecbe4704932ab016b3bb07296
keithmoore1 <keith_moore7@msn.com>
2019-08-08 10:13:54 -0600

    Day 4

--- generator_objects.py
+++ generator_objects.py
@@ -15,10 +15,10 @@ def main():
     m_sq = (x*x for x in range(1,1000001))
     print (m_sq, type(m_sq))
     print("The sum of the first 1M squared numbers is: ", sum(m_sq))
-    print("The sum of the first 1M squared numbers is: ", sum(x*x for x in range(1,10000)))
+    print("The sum of the first 1M squared numbers is: ", sum(x*x for x in range(1,1000000)))
 
     # the sum of the prime numbers between 1 to 10K
-    print("The sum of the first 10K squared numbers is: ", sum(x for x in range(1, 10000) if is_prime(x)))
+    print("The sum of the first 10K squared numbers is: ", sum(x for x in range(1, 10001) if is_prime(x)))
 
 
 
